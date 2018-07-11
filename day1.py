6f58765d4dcc2c727a5ce74ff4f2aadf7fed7998
priteshchhabra <36761974+priteshchhabra@users.noreply.github.com>
2018-07-11 12:31:18 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,12 +1,16 @@
-n = int(input("please enter the number of fractions: "))
-for i in range(n):
-    x = int(input(" please enter your %d fraction",i))
-    if i==1:
-        sum = x
-        print(x)
-    else:
-        sum += x
-        print("+ ",x)
-print("= +",sum)
+from fractions import Fraction
+class instance:
+    def __init__(self,n):
+        self.n=n
+    def instances(self):
+        self.n = int(input("please enter the number of fractions: "))
+        return(self.n)  
         
-        
+def compute():
+    n = instance
+    sum = 0
+    for i in range(n.instances(n)):
+        x = int(input(" please enter your %d fraction"%(i+1)))
+        sum += 1/x
+    return Fraction(sum.as_integer_ratio())
+print(compute())    
